15b9416cae243c34b40924e8ff1f033e17fb3817
fabs <faebeee@gmail.com>
2017-06-06 18:13:23 +0200

    transparent footer

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
     <div>
         <router-view></router-view>
 
-        <footer class="page-footer">
+        <footer class="page-footer transparent">
             <div class="footer-copyright">
                 <div class="container">
-                    © 2017 <a class="grey-text text-lighten-4" href="https://fabs.io">Fabs.io</a>
-                    <a class="grey-text text-lighten-4 right" href="https://github.com/faebeee/arnis-interval">GitHub</a>
+                    <a class="grey-text text-darken-4" href="https://fabs.io">&copy; 2017 Fabs.io</a>
+                    <a class="grey-text text-darken-4 right" href="https://github.com/faebeee/arnis-interval">GitHub</a>
 
                 </div>
             </div>
